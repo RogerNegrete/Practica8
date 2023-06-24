1765c8a8526c0147156c2cfc15c875b9770dd1f7
Roger Negrete <rnegretec@est.ups.edu.ec>
2023-06-24 09:59:51 -0500

    Ultima actualizacion

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,12 +14,20 @@ MainWindow::~MainWindow()
 }
 
 
+
 void MainWindow::on_pushButton_clicked()
 {
     QString directory = QFileDialog::getExistingDirectory(this, tr("Open Directory"), "/home/alumno", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
     if (!directory.isEmpty()) {
-        // Do something with the selected directory
-        ui->label->setText(directory); //Muestra la ruta en el label
+        ui->label->setText(directory); // Establecer la ruta de la carpeta en el QLabel
+
+        QDir dir(directory);
+        QStringList entries = dir.entryList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot); // Obtener carpetas y archivos dentro del directorio
+
+        ui->listWidget->clear(); // Limpiar la lista antes de agregar nuevos elementos
+
+        for (const QString& entry : entries) {
+            ui->listWidget->addItem(entry); // Agregar cada carpeta o archivo a la lista
+        }
     }
 }
-
